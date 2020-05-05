e4327b1cb6a602f176d0a9bbcd997e63ddc28450
JuliBaz <61162307+JuliBaz@users.noreply.github.com>
2020-05-06 00:10:18 +0300

    task1-3

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,11 +1,30 @@
 package subtask4
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
         if (number<=4) return null
-        throw NotImplementedError("Not implemented")
+        return recursion(number*number, number - 1)
+    }
+    fun recursion(value:Int,konec:Int): Array<Int>? {
+        for (i in konec downTo 1){
+            val square = i*i
+            val difference = value - square
+            if (difference==0){
+                return arrayOf(i)
+            }
+            if (difference<0){
+                return null
+            }
+            var sqrt = sqrt(difference.toDouble()).toInt()
+            if(sqrt>=i) sqrt = i-1
+            var chast = recursion(difference,sqrt)
+            if(chast!=null)  return chast+ arrayOf(i)
+        }
+        return null
+    }
 
     }
-}
